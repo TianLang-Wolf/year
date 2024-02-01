0b45e413b466a9d48d9968835b0fe72b22d278a9
kaersie <kaersie@163.com>
2024-02-01 15:33:57 +0800

    'new'

--- change.js
+++ change.js
@@ -14,21 +14,29 @@
   })();
 
   function readname(){
-    let url;
-    url = window.location.hash; 
-    url=url.slice(1);
-    url=decodeURIComponent(url);
-    if (url==""){
-        i=1
+    let url=location.search;
+    let strs;
+    let str;
+    let name;
+    if(url.indexOf("?")!=-1){
+        str=url.substr(1);
+        strs=str.split("&");
+        name=decodeURIComponent(strs[0].replace("name=",""));
+    }
+    if (name==""| name==null){
+        i=1;
         while(i==1){
-            url=prompt("怎么称呼您？");
-        if (url!=null&url!="") {
+            name=prompt("怎么称呼您？");
+        if (name!=null&name!="") {
             i=2;   
         }else{alert("姓名不能为空");}
         }
     }
-    return url;
+    return name;
 }
+
+
+
 function showit(){
     let first1=document.getElementById("first");
     console.log(first1);
@@ -38,8 +46,13 @@ function showit(){
 
 }
 showit();
-
-function urlq(){
-    location.href = "http://kaersie.great-site.net";
-}
-console.log("version: 0.2");
+async function showagain() {
+    const res = await fetch('data.json');
+    const data = await res.json();
+    //   随机取出data的某一项
+    const randomIndex = Math.floor(Math.random() * data.length);
+    const randomtext = data[randomIndex].text;
+      let b=document.getElementById("second");
+      b.innerText=randomtext;
+  }
+  showagain();
